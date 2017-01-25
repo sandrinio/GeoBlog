a0803599be5965f4b5d9cc77127fe3447278109a
sandrinio <sandro.suladze@gmail.com>
2017-01-26 01:22:06 +0400

    "req.flash"

--- routes/auth.js
+++ routes/auth.js
@@ -20,9 +20,11 @@ router.post("/new", function (req, res) {
    User.register(req.body.user, req.body.password, function (err, user) {
       if(err){
           console.log(err);
+          req.flash("error", err);
           return res.render("auth/new");
       }
       passport.authenticate("local")(req, res, function () {
+          req.flash("success", "User Created: " + user.firstname + " " + (user.lastname))
            res.redirect("/mainPage");
        })
    });
@@ -30,14 +32,15 @@ router.post("/new", function (req, res) {
 
 router.post("/", passport.authenticate("local", {
     successRedirect: "/mainPage",
-    failureRedirect: "/"
+    failureRedirect: "/",
 }), function (req, res) {
+    req.flash("success", "Welcome " + user.firstname + " " + (user.lastname));
     res.redirect("mainPage/mainPage");
 });
 
 router.get("/logout", function (req, res) {
     req.logout();
-    //req.flash("success", "Logged Out");
+    req.flash("success", "Logged Out");
     res.redirect("/");
 });
 
--- routes/mainPage.js
+++ routes/mainPage.js
@@ -54,8 +54,42 @@ router.get("/show", middleware.isLoggedIn, function (req, res) {
         res.send("coming soon")
 });
 
+//  ================== ADMIN PARTS ==================
+router.get("/mainPage/:id/edit", middleware.permissionChecker, function (req, res) {
+    Posts.findById(req.params.id, function (err, cPost) {
+        if(err){
+            req.flash(err);
+            res.redirect("back");
+        }else {
+            res.render("mainPage/edit", {cPost: cPost})
+        }
+    });
+});
+
+router.put("/mainPage/:id", function (req, res) {
+    Posts.findByIdAndUpdate(req.params.id, req.body.cPost, function (err, changedPost) {
+        if(err){
+            req.flash("error", err);
+            res.redirect("back")
+        }else{
+            req.flash("success", "Post Was modified")
+            res.redirect("back");
+        }
+    })
+});
 
 
+
+router.delete("/mainPage/:id", middleware.permissionChecker, function (req, res) {
+   Posts.findByIdAndRemove(req.params.id, function (err, blogPost) {
+      if(err){
+      console.log(err)
+      }else{
+          req.flash("success", blogPost.title + " " + "has been removed")
+          res.redirect("/mainPage");
+      }
+   });
+});
 
 
 module.exports = router;
